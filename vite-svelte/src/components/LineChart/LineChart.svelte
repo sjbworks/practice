4c10cd9e0dcd465246042283578f0a4ef3d9459e
sjbworks <sacred.jizo.bosatz@gmail.com>
2022-01-01 00:21:43 +0900

    feat: add components/LieChart in vite-svelte

---
+++ vite-svelte/src/components/LineChart/LineChart.svelte
@@ -0,0 +1,14 @@
+<script lang="ts" generic="T">
+import Chart from "svelte-apexcharts";
+import type { LineChartProps } from ".";
+
+export let props: LineChartProps<T>  = {
+  options: {},
+  series: [{ name: "", data: [] }],
+  className: "",
+}
+</script>
+
+<div class={$$props.className}>
+  <Chart series={props.series} options={props.options} type="line" />
+</div>
