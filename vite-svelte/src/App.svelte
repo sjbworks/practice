797c2aeff8a60593f15bcca5a988ba925d021449
sjbworks <sacred.jizo.bosatz@gmail.com>
2022-01-01 00:20:49 +0900

    feat: create vite-svelte

---
+++ vite-svelte/src/App.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+  import logo from './assets/svelte.png'
+  import Counter from './lib/Counter.svelte'
+  import {Card} from './components/Card'
+  import {LineChart,LineChartSeries} from './components/LineChart'
+
+  const PERIOD = {
+    MONTH: "month",
+    HALF_YEAR: "halfYear",
+    YEAR: "year",
+    ALL: "all",
+  };
+
+  let data:LineChartSeries<number>[]
+  let selectValue:string = "month"
+
+  const periodMap = {
+    [PERIOD.MONTH]: {
+      from: "2020-03-01T00:00:00Z",
+      to: "2020-04-01T00:00:00Z",
+    },
+    [PERIOD.HALF_YEAR]: {
+      from: "2020-03-01T00:00:00Z",
+      to: "2020-09-01T00:00:00Z",
+    },
+    [PERIOD.YEAR]: { from: "2020-03-01T00:00:00Z", to: "2021-03-01T00:00:00Z" },
+    [PERIOD.ALL]: { from: "2020-03-01T00:00:00Z", to: "2021-12-28T00:00:00Z" },
+  };
+
+  const setGraphData = async () => {
+    const newData = await getGraphData(
+      periodMap[selectValue].from,
+      periodMap[selectValue].to
+    );
+    const data = newData.map(({ Cases }: S) => Cases);
+    setData([{ name: "s", data }]);
+  };
+
+  // useEffect(() => {
+  //   setGraphData();
+  //   console.log(data);
+  // }, [selectValue]);
+
+</script>
+
+<main>
+  <Card><LineChart generic={Number} /></Card>
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
+
+  
+</style>
