0f39134dd1984f246877eb3343cf6a3febfbf190
sjbworks <sacred.jizo.bosatz@gmail.com>
2022-01-04 02:06:28 +0900

    feat: create src/types

---
+++ vite-svelte/vite-svelte/src/types/index.ts
@@ -0,0 +1,12 @@
+export type S = {
+  Cases: number
+  City: string
+  CityCode: string
+  Country: string
+  CountryCode: string
+  Date: string
+  Lat: string
+  Lon: string
+  Province: string
+  Status: string
+}
