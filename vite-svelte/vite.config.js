8a3ba30d87cf9c61eeeef426f906f52ec6d08463
sjbworks <sacred.jizo.bosatz@gmail.com>
2022-01-05 02:44:21 +0900

    edit: vite.comfig.js[wip]

--- vite-svelte/vite.config.js
+++ vite-svelte/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import preprocess from "svelte-preprocess";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +8,27 @@ export default defineConfig({
   build: {
     outDir: "../public",
   },
-  plugins: [svelte()],
+  plugins: [svelte({ emitCss: false, preprocess: preprocess() })],
 });
+
+// import { defineConfig } from "vite";
+// import { svelte } from "@sveltejs/vite-plugin-svelte";
+// import tsconfigPaths from "vite-tsconfig-paths";
+// // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => {
+//   const isProduction = mode === "production";
+//   return {
+//     root: "./src",
+//     build: {
+//       outDir: "../public",
+//       minify: isProduction,
+//     },
+//     plugins: [
+//       svelte({
+//         hot: !isProduction,
+//       }),
+//       tsconfigPaths(),
+//     ],
+//     publicDir: "../public",
+//   };
+// });
