c1269921f3ca63085babe2d61ed42b43cc3995ff
sjbworks <sacred.jizo.bosatz@gmail.com>
2022-05-02 18:41:22 +0900

    median

--- typesctipt/index.ts
+++ typesctipt/index.ts
@@ -26,3 +26,16 @@ const addTwoNumbers = (l1: any[], l2: any[]) => {
 
 console.log(addTwoNumbers([2, 4, 3], [5, 6, 4]));
 console.log(addTwoNumbers([9, 9, 9, 9, 9, 9, 9], [9, 9, 9, 9]));
+
+// 中央値を求める
+function getMedian(nums1: number[], nums2: number[]): number {
+  let mergedArray = [...nums1, ...nums2];
+  const sorted = mergedArray.sort((a: number, b: number) => a - b);
+  const middle = Math.floor(sorted.length / 2);
+  return sorted.length % 2 === 0
+    ? (sorted[middle - 1] + sorted[middle]) / 2.0
+    : sorted[middle];
+}
+
+console.log(getMedian([1, 3], [2])); // 2
+console.log(getMedian([3], [-2, -1])); // -1
