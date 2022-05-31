d94284e213c7789cf0b7649ee0be6f9d51e8a491
sjbworks <sacred.jizo.bosatz@gmail.com>
2022-06-01 00:25:22 +0900

    zigzag

--- typesctipt/index.ts
+++ typesctipt/index.ts
@@ -89,3 +89,18 @@ function expandAroundCenter(s: string, left: number, right: number): number {
   }
   return r - l - 1;
 }
+
+function convert(s: string, numRows: number): string {
+  if(numRows < 2) return s
+  let zig = []
+  let cycle = numRows * 2 - 2
+  for(let j = 0; j < numRows; j++){
+      for(let i = 0; j + i < s.length; i+= cycle) {
+          zig.push(s.charAt(i+j))
+          if(j !== 0 && j !== numRows-1) {
+              zig.push(s.charAt(i + cycle -j))
+          }
+      }
+  }
+  return zig.join('')
+};
