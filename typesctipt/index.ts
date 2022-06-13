d2f28de2f7db66387dcfe7f10f692473e7b216db
sjbworks <sacred.jizo.bosatz@gmail.com>
2022-06-13 23:31:34 +0900

    clear isMatch

--- typesctipt/index.ts
+++ typesctipt/index.ts
@@ -118,3 +118,7 @@ function isPalindrome(x: number): boolean {
   if (x < 0) false;
   return x === Number(String(x).split("").reverse().join("")) ? true : false;
 }
+
+function isMatch(s: string, p: string): boolean {
+  return new RegExp(`^${p}$`).test(s);
+}
