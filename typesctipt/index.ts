12f23be2918fb6d900232f6af6f571944b455dae
sjbworks <sacred.jizo.bosatz@gmail.com>
2022-06-23 00:17:33 +0900

    maxArea

--- typesctipt/index.ts
+++ typesctipt/index.ts
@@ -122,3 +122,21 @@ function isPalindrome(x: number): boolean {
 function isMatch(s: string, p: string): boolean {
   return new RegExp(`^${p}$`).test(s);
 }
+
+function maxArea(height: number[]): number {
+  const { length } = height;
+  let left = 0;
+  let right = length - 1;
+  let max = 0;
+
+  while (left < right) {
+    const x = right - left;
+    const y = height[height[left] < height[right] ? left++ : right--];
+
+    const res = x * y;
+
+    if (res > max) max = res;
+  }
+
+  return max;
+}
