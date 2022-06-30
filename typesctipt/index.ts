127c67a56aebbdf4b084ce21f9e2b397e5e9b273
sjbworks <sacred.jizo.bosatz@gmail.com>
2022-06-30 23:12:46 +0900

    clear intToRoman

--- typesctipt/index.ts
+++ typesctipt/index.ts
@@ -141,3 +141,33 @@ function maxArea(height: number[]): number {
 
   return max;
 }
+
+function intToRoman(num: number): string {
+  const romans = [
+    "M",
+    "CM",
+    "D",
+    "CD",
+    "C",
+    "XC",
+    "L",
+    "XL",
+    "X",
+    "IX",
+    "V",
+    "IV",
+    "I",
+  ];
+  const values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  let res = "";
+  for (let i = 0; i <= values.length; i++) {
+    while (num >= values[i]) {
+      num -= values[i];
+      res += romans[i];
+    }
+  }
+  return res;
+}
+
+console.log(intToRoman(58)); // LVIII
+console.log(intToRoman(1994)); // MCMXCIV
